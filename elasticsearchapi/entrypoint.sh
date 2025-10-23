3a762f6debe73280fe2b28110fbf5d76551e8887
MD. ABDULLA AL MAMUN <abdulla.mamun@netmediamobile.com>
2025-10-23 22:57:29 +0600

    dockerfile and entrypoint fixed

--- elasticsearchapi/entrypoint.sh
+++ elasticsearchapi/entrypoint.sh
@@ -18,17 +18,17 @@ echo "Postgres is ready!"
 
 echo "Waiting for Elasticsearch..."
 elapsed=0
-# while ! curl -s http://localhost:9200 >/dev/null 2>&1 && [ $elapsed -lt $timeout ]; do
-#   echo "Waiting for Elasticsearch... ($elapsed/$timeout)"
-#   sleep 2
-#   elapsed=$((elapsed + 2))
-# done
+while ! curl -s http://esearch:9200 >/dev/null 2>&1 && [ $elapsed -lt $timeout ]; do
+  echo "Waiting for Elasticsearch... ($elapsed/$timeout)"
+  sleep 2
+  elapsed=$((elapsed + 2))
+done
 
-# if [ $elapsed -ge $timeout ]; then
-#   echo "Elasticsearch did not start within $timeout seconds."
-#   exit 1
-# fi
-# echo "Elasticsearch is ready!"
+if [ $elapsed -ge $timeout ]; then
+  echo "Elasticsearch did not start within $timeout seconds."
+  exit 1
+fi
+echo "Elasticsearch is ready!"
 
 echo "Running collectstatic..."
 python manage.py collectstatic --noinput
